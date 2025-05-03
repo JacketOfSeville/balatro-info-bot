8a24a75d3364af3666140af0b950900221129030
JacketOfSeville <jacketfollowme@gmail.com>
2025-05-03 11:55:04 -0300

    feat: tarot images added

--- src/lib/tarots.py
+++ src/lib/tarots.py
@@ -1,90 +1,112 @@
 tarots = {
     "t_chariot": {
         "name": "The Chariot",
-        "text": "Enhances 1 selected card into a Steel card"
+        "text": "Enhances 1 selected card into a Steel card",
+        "image": "https://static.wikia.nocookie.net/balatrogame/images/f/f8/Tarot_The_Chariot.png"
     },
     "t_death": {
         "name": "Death",
-        "text": "Select 2 cards, convert the left card into the right card (Drag to rearrange)"
+        "text": "Select 2 cards, convert the left card into the right card (Drag to rearrange)",
+        "image": "https://static.wikia.nocookie.net/balatrogame/images/4/4d/Tarot_Death.png"
     },
     "t_devil": {
         "name": "The Devil",
-        "text": "Enhances 1 selected card into a Gold card"
+        "text": "Enhances 1 selected card into a Gold card",
+        "image": "https://static.wikia.nocookie.net/balatrogame/images/7/72/Tarot_The_Devil.png"
     },
     "t_emperor": {
         "name": "The Emperor",
-        "text": "Creates up to 2 random Tarot cards (Must have room)"
+        "text": "Creates up to 2 random Tarot cards (Must have room)",
+        "image": "https://static.wikia.nocookie.net/balatrogame/images/1/11/Tarot_The_Emperor.png"
     },
     "t_empress": {
         "name": "The Empress",
-        "text": "Enhances 2 selected cards into Mult cards"
+        "text": "Enhances 2 selected cards into Mult cards",
+        "image": "https://static.wikia.nocookie.net/balatrogame/images/8/8b/Tarot_The_Empress.png"
     },
     "t_fool": {
         "name": "The Fool",
-        "text": "Creates the last Tarot or Planet card used during this run (The Fool excluded)"
+        "text": "Creates the last Tarot or Planet card used during this run (The Fool excluded)",
+        "image": "https://static.wikia.nocookie.net/balatrogame/images/6/61/Tarot_The_Fool.png"
     },
     "t_hanged_man": {
         "name": "The Hanged Man",
-        "text": "Destroys up to 2 selected cards"
+        "text": "Destroys up to 2 selected cards",
+        "image": "https://static.wikia.nocookie.net/balatrogame/images/d/d6/Tarot_The_Hanged_Man.png"
     },
-    "t_heirophant": {
+    "t_hierophant": {
         "name": "The Hierophant",
-        "text": "Enhances 2 selected cards to Bonus cards"
+        "text": "Enhances 2 selected cards to Bonus cards",
+        "image": "https://static.wikia.nocookie.net/balatrogame/images/b/bc/Tarot_The_Hierophant.png"
     },
     "t_hermit": {
         "name": "The Hermit",
-        "text": "Doubles money (Max of $20)"
+        "text": "Doubles money (Max of $20)",
+        "image": "https://static.wikia.nocookie.net/balatrogame/images/1/19/Tarot_The_Hermit.png"
     },
     "t_high_priestess": {
         "name": "The High Priestess",
-        "text": "Creates up to 2 random Planet cards (Must have room)"
+        "text": "Creates up to 2 random Planet cards (Must have room)",
+        "image": "https://static.wikia.nocookie.net/balatrogame/images/c/cc/Tarot_The_High_Priestess.png"
     },
     "t_judgement": {
         "name": "Judgement",
-        "text": "Creates a random Joker card (Must have room)"
+        "text": "Creates a random Joker card (Must have room)",
+        "image": "https://static.wikia.nocookie.net/balatrogame/images/d/d9/Tarot_Judgement.png"
     },
     "t_justice": {
         "name": "Justice",
-        "text": "Enhances 1 selected card into a Glass card"
+        "text": "Enhances 1 selected card into a Glass card",
+        "image": "https://static.wikia.nocookie.net/balatrogame/images/4/45/Tarot_Justice.png"
     },
     "t_lovers": {
         "name": "The Lovers",
-        "text": "Enhances 1 selected card into a Wild card"
+        "text": "Enhances 1 selected card into a Wild card",
+        "image": "https://static.wikia.nocookie.net/balatrogame/images/4/4c/Tarot_The_Lovers.png"
     },
     "t_magician": {
         "name": "The Magician",
-        "text": "Enhances 2 selected cards into Lucky cards"
+        "text": "Enhances 2 selected cards into Lucky cards",
+        "image": "https://static.wikia.nocookie.net/balatrogame/images/7/78/Tarot_The_Magician.png"
     },
     "t_moon": {
         "name": "The Moon",
-        "text": "Converts up to 3 selected cards to Clubs"
+        "text": "Converts up to 3 selected cards to Clubs",
+        "image": "https://static.wikia.nocookie.net/balatrogame/images/2/29/Tarot_The_Moon.png"
     },
     "t_star": {
         "name": "The Star",
-        "text": "Converts up to 3 selected cards to Diamonds"
+        "text": "Converts up to 3 selected cards to Diamonds",
+        "image": "https://static.wikia.nocookie.net/balatrogame/images/e/e1/Tarot_The_Star.png"
     },
     "t_strength": {
         "name": "Strength",
-        "text": "Increases rank of up to 2 selected cards by 1"
+        "text": "Increases rank of up to 2 selected cards by 1",
+        "image": "https://static.wikia.nocookie.net/balatrogame/images/7/7d/Tarot_Strength.png"
     },
     "t_sun": {
         "name": "The Sun",
-        "text": "Converts up to 3 selected cards to Hearts"
+        "text": "Converts up to 3 selected cards to Hearts",
+        "image": "https://static.wikia.nocookie.net/balatrogame/images/2/2e/Tarot_The_Sun.png"
     },
     "t_temperance": {
         "name": "Temperance",
-        "text": "Gives the total sell value of all current Jokers (Max of $50)"
+        "text": "Gives the total sell value of all current Jokers (Max of $50)",
+        "image": "https://static.wikia.nocookie.net/balatrogame/images/6/67/Tarot_Temperance.png"
     },
     "t_tower": {
         "name": "The Tower",
-        "text": "Enhances 1 selected card into a Stone card"
+        "text": "Enhances 1 selected card into a Stone card",
+        "image": "https://static.wikia.nocookie.net/balatrogame/images/a/af/Tarot_The_Tower.png"
     },
     "t_wheel_of_fortune": {
         "name": "The Wheel of Fortune",
-        "text": "1 in 4 chance to add Foil, Holographic, or Polychrome edition to a random Joker"
+        "text": "1 in 4 chance to add Foil, Holographic, or Polychrome edition to a random Joker",
+        "image": "https://static.wikia.nocookie.net/balatrogame/images/5/57/Tarot_The_Wheel_of_Fortune.png"
     },
     "t_world": {
         "name": "The World",
-        "text": "Converts up to 3 selected cards to Spades"
+        "text": "Converts up to 3 selected cards to Spades",
+        "image": "https://static.wikia.nocookie.net/balatrogame/images/4/49/Tarot_The_World.png"
     }
 }
