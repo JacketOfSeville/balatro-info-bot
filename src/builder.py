1233de8dd396d975ceb9d7fa499fa4d2c6c2fb25
= <luigi.cfranca@gmail.com>
2025-05-04 16:32:20 -0300

    refactor: split build_reply in multiple functions
    Split the `build_reply` function in `builder.py`
    into three smaller functions in order to apply the
    SRP (Single Responsibility Principle).

--- src/builder.py
+++ src/builder.py
@@ -15,22 +15,50 @@ CATEGORIES_TO_DATASETS = {
     'planets': planets
 }
 
+LOWEST_DIFFERENCE_TO_MATCH = 5
+
+BOSS_ID_PREFIX = 'bl_final_'
+
+def flatten_and_find_last(map, func):
+    """
+    Looks for the last element in a nested dictionary that matches a condition.
+
+    Returns the last element that matches the condition, or None if no match is found.
+    """
+    
+    result = None
+    
+    for k1, v1 in map.items():
+        for k2, v2 in v1.items():
+            if func(v2):
+                result = (v2, k2, k1)
+    
+    return result
+
+def find_best_match(search_term: str):
+    lowest_difference_score = LOWEST_DIFFERENCE_TO_MATCH
+
+    def is_best_match(card_data):
+        nonlocal lowest_difference_score
+        card_name = card_data.get("name", "").lower()
+        name_difference_score = levenshtein_distance(card_name, search_term)
+
+        if name_difference_score >= lowest_difference_score:
+            return False
+
+        lowest_difference_score = name_difference_score
+        return True
+
+    return flatten_and_find_last(CATEGORIES_TO_DATASETS, is_best_match)
+
 def build_reply(search_term: str):
-    input = search_term.lower()
-    lowest_difference_score = float('inf')
-    best_match = None
+    best_match, best_match_id, best_match_category = find_best_match(search_term)
 
-    for category, dataset in CATEGORIES_TO_DATASETS.items():
-        for card_id, card_data in dataset.items():
-            card_name = card_data.get("name", "").lower()
-            name_difference_score = levenshtein_distance(card_name, input)
-
-            if name_difference_score < lowest_difference_score:
-                lowest_difference_score = name_difference_score
-                best_match = card_data.copy()
-                best_match["category"] = category
-
-                if 'bl_final' in card_id:
-                    best_match["boss"] = 'Final '
-
-    return None if lowest_difference_score > 5 else best_match
+    if best_match is None:
+        return None
+    
+    return {
+        **best_match,
+        "category": best_match_category,
+        "boss": 'Final ' if BOSS_ID_PREFIX in best_match_id else None,
+    }
