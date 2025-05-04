50cf95ec06f14ebfb6e196c5f9e42e91001dec16
= <luigi.cfranca@gmail.com>
2025-05-04 18:34:02 -0300

    refactor: generalize logic to build the embedings

--- src/main.py
+++ src/main.py
@@ -14,7 +14,76 @@ FOOTER = 'Data extracted from game files'
 DEFALT_RARITY = 'Common'
 DEFALT_UNLOCK = 'Available from the start'
 
+CATEGORIES_TO_COLORS = {
+    "jokers": discord.Colour.red(),
+    "vouchers": discord.Colour.green(),
+    "blinds": discord.Colour.orange(),
+    "tarots": discord.Colour.purple(),
+    "spectrals": discord.Colour.dark_blue(),
+    "planets": discord.Colour.blue()
+}
+
+def append_unlocked_by(card, _):
+    return discord.Embed(
+        colour=discord.Colour.red(),
+        description=card.get('rarity', DEFALT_RARITY)+' Joker',
+        title=card['name']
+    ).add_field(
+        name='Unlocked by',
+        value=card.get('unlock', DEFALT_UNLOCK)
+    )
+
+def append_embed_for_blinds(blind, _):
+    return discord.Embed(
+        colour=discord.Colour.orange(),
+        description=blind.get('boss', '')+'Boss Blind',
+        title=blind['name']
+    )
+
+CATEGORIES_TO_EMBED_APPENDER = {
+    "jokers": append_unlocked_by,
+    "vouchers": append_unlocked_by,
+    "blinds": append_embed_for_blinds,
+}
+
+def build_embed_for_unknown(match):
+    embed = discord.Embed(
+        colour=discord.Colour.darker_grey(),
+        description='Item not found in the database',
+        title=match
+    )
+
+    return embed
+
+def build_embed_for_card(card):
+    card_category = card['category']
+
+    embed = discord.Embed(
+        colour=CATEGORIES_TO_COLORS[card_category],
+        description=card_category,
+        title=card['name']
+    )
+
+    appender = CATEGORIES_TO_EMBED_APPENDER.get(card_category)
+    if appender is not None:
+        embed = appender(card, embed)
+
+    image = card.get('image')
+    if image is not None:
+        embed.set_thumbnail(url=image)
+
+    embed.set_footer(text=FOOTER)
+    embed.set_author(name=AUTHOR)
+    embed.add_field(name='Effect', value=card['text'])
+
+    return embed
+
 class Client(discord.Client):
+    def choose_embed(self, match):
+        card = build_reply(match)
+
+        return build_embed_for_unknown(match) if card is None else build_embed_for_card(card)
+
     async def on_ready(self):
         print(f'Logged in as {self.user}')
 
@@ -23,137 +92,13 @@ class Client(discord.Client):
             return
         
         matches = re.findall(r'\[\[(.*?)\]\]', message.content)
-        if matches:
-            embeds = []
 
-            for match in matches:
-                print(f'Deteced: {match}')
-                result = build_reply(match)
-                if result:
-                    if result['category'] == 'jokers':
-                        rarity = result.get('rarity', DEFALT_RARITY)
-                        unlock = result.get('unlock', DEFALT_UNLOCK)
-                        image = result.get('image')
-
-                        embed = discord.Embed(
-                            colour=discord.Colour.red(),
-                            description = rarity+' Joker',
-                            title = result['name']
-                        )
-                        if image:
-                            embed.set_thumbnail(url = result['image'])
-
-                        embed.set_footer(text = FOOTER)
-                        embed.set_author(name = AUTHOR)
-                        
-                        embed.add_field(name = 'Effect',value = result['text'])
-                        embed.add_field(name = 'Unlocked by',value = unlock)
-
-                        embeds.append(embed)
-                    elif result['category'] == 'vouchers':
-                        unlock = result.get('unlock', DEFALT_UNLOCK)
-                        image = result.get('image')
-
-                        embed = discord.Embed(
-                            colour=discord.Colour.green(),
-                            description = 'Voucher',
-                            title = result['name']
-                        )
-                        if image:
-                            embed.set_thumbnail(url = result['image'])
-
-                        embed.set_footer(text = FOOTER)
-                        embed.set_author(name = AUTHOR)
-                        
-                        embed.add_field(name = 'Effect',value = result['text'])
-                        embed.add_field(name = 'Unlocked by',value = unlock)
-
-                        embeds.append(embed)
-                    elif result['category'] == 'blinds':
-                        image = result.get('image')
-                        boss = result.get('boss', '')
-
-                        embed = discord.Embed(
-                            colour=discord.Colour.orange(),
-                            description = boss+'Boss Blind',
-                            title = result['name']
-                        )
-                        if image:
-                            embed.set_thumbnail(url = result['image'])
-
-                        embed.set_footer(text = FOOTER)
-                        embed.set_author(name = AUTHOR)
-                        
-                        embed.add_field(name = 'Effect',value = result['text'])
-
-                        embeds.append(embed)
-                    elif result['category'] == 'tarots':
-                        image = result.get('image')
-
-                        embed = discord.Embed(
-                            colour=discord.Colour.purple(),
-                            description = 'Tarot Card',
-                            title = result['name']
-                        )
-                        if image:
-                            embed.set_thumbnail(url = result['image'])
-
-                        embed.set_footer(text = FOOTER)
-                        embed.set_author(name = AUTHOR)
-                        
-                        embed.add_field(name = 'Effect',value = result['text'])
-
-                        embeds.append(embed)
-                    elif result['category'] == 'spectrals':
-                        image = result.get('image')
-
-                        embed = discord.Embed(
-                            colour=discord.Colour.dark_blue(),
-                            description = 'Spectral Card',
-                            title = result['name']
-                        )
-                        if image:
-                            embed.set_thumbnail(url = result['image'])
-
-                        embed.set_footer(text = FOOTER)
-                        embed.set_author(name = AUTHOR)
-                        
-                        embed.add_field(name = 'Effect',value = result['text'])
-
-                        embeds.append(embed)
-                    elif result['category'] == 'planets':
-                        image = result.get('image')
-
-                        embed = discord.Embed(
-                            colour=discord.Colour.blue(),
-                            description = 'Planet Card',
-                            title = result['name']
-                        )
-                        if image:
-                            embed.set_thumbnail(url = result['image'])
-
-                        embed.set_footer(text = FOOTER)
-                        embed.set_author(name = AUTHOR)
-                        
-                        embed.add_field(name = 'Effect',value = result['text'])
-
-                        embeds.append(embed)
-                    else:
-                        print('Unknown Item')
-                else:
-                    embed = discord.Embed(
-                        colour=discord.Colour.darker_grey(),
-                        description = 'Item not found in the database',
-                        title = match
-                    )
-
-                    embed.set_footer(text = FOOTER)
-                    embed.set_author(name = AUTHOR)
-
-                    embeds.append(embed)                    
-
-            await message.reply(embeds=embeds[:10])
+        if not matches:
+            return
         
+        embeds = [self.choose_embed(match) for match in matches]
+
+        await message.reply(embeds=embeds[:10])
 
 intents = discord.Intents.default()
 intents.message_content = True
