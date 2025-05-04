42f917355587736b8825d0ef0e5745af37991209
= <luigi.cfranca@gmail.com>
2025-05-04 17:13:59 -0300

    style: replace double quotes with single quotes

--- src/main.py
+++ src/main.py
@@ -16,130 +16,130 @@ class Client(discord.Client):
         if message.author.bot:
             return
         
-        matches = re.findall(r"\[\[(.*?)\]\]", message.content)
+        matches = re.findall(r'\[\[(.*?)\]\]', message.content)
         if matches:
             embeds = []
-            author = "Balatro Information Bot"
-            footer = "Data extracted from game files"
+            author = 'Balatro Information Bot'
+            footer = 'Data extracted from game files'
 
             for match in matches:
-                print(f"Deteced: {match}")
+                print(f'Deteced: {match}')
                 result = build_reply(match)
                 if result:
-                    if result["category"] == 'jokers':
+                    if result['category'] == 'jokers':
                         rarity = result.get('rarity', 'Common')
                         unlock = result.get('unlock', 'Available from the start')
                         image = result.get('image')
 
                         embed = discord.Embed(
                             colour=discord.Colour.red(),
-                            description = rarity+" Joker",
+                            description = rarity+' Joker',
                             title = result['name']
                         )
                         if image:
-                            embed.set_thumbnail(url = result["image"])
+                            embed.set_thumbnail(url = result['image'])
 
                         embed.set_footer(text = footer)
                         embed.set_author(name = author)
                         
-                        embed.add_field(name = "Effect",value = result["text"])
-                        embed.add_field(name = "Unlocked by",value = unlock)
+                        embed.add_field(name = 'Effect',value = result['text'])
+                        embed.add_field(name = 'Unlocked by',value = unlock)
 
                         embeds.append(embed)
-                    elif result["category"] == 'vouchers':
+                    elif result['category'] == 'vouchers':
                         unlock = result.get('unlock', 'Available from the start')
                         image = result.get('image')
 
                         embed = discord.Embed(
                             colour=discord.Colour.green(),
-                            description = "Voucher",
+                            description = 'Voucher',
                             title = result['name']
                         )
                         if image:
-                            embed.set_thumbnail(url = result["image"])
+                            embed.set_thumbnail(url = result['image'])
 
                         embed.set_footer(text = footer)
                         embed.set_author(name = author)
                         
-                        embed.add_field(name = "Effect",value = result["text"])
-                        embed.add_field(name = "Unlocked by",value = unlock)
+                        embed.add_field(name = 'Effect',value = result['text'])
+                        embed.add_field(name = 'Unlocked by',value = unlock)
 
                         embeds.append(embed)
-                    elif result["category"] == 'blinds':
+                    elif result['category'] == 'blinds':
                         image = result.get('image')
                         boss = result.get('boss', '')
 
                         embed = discord.Embed(
                             colour=discord.Colour.orange(),
-                            description = boss+"Boss Blind",
+                            description = boss+'Boss Blind',
                             title = result['name']
                         )
                         if image:
-                            embed.set_thumbnail(url = result["image"])
+                            embed.set_thumbnail(url = result['image'])
 
                         embed.set_footer(text = footer)
                         embed.set_author(name = author)
                         
-                        embed.add_field(name = "Effect",value = result["text"])
+                        embed.add_field(name = 'Effect',value = result['text'])
 
                         embeds.append(embed)
-                    elif result["category"] == 'tarots':
+                    elif result['category'] == 'tarots':
                         image = result.get('image')
 
                         embed = discord.Embed(
                             colour=discord.Colour.purple(),
-                            description = "Tarot Card",
+                            description = 'Tarot Card',
                             title = result['name']
                         )
                         if image:
-                            embed.set_thumbnail(url = result["image"])
+                            embed.set_thumbnail(url = result['image'])
 
                         embed.set_footer(text = footer)
                         embed.set_author(name = author)
                         
-                        embed.add_field(name = "Effect",value = result["text"])
+                        embed.add_field(name = 'Effect',value = result['text'])
 
                         embeds.append(embed)
-                    elif result["category"] == 'spectrals':
+                    elif result['category'] == 'spectrals':
                         image = result.get('image')
 
                         embed = discord.Embed(
                             colour=discord.Colour.dark_blue(),
-                            description = "Spectral Card",
+                            description = 'Spectral Card',
                             title = result['name']
                         )
                         if image:
-                            embed.set_thumbnail(url = result["image"])
+                            embed.set_thumbnail(url = result['image'])
 
                         embed.set_footer(text = footer)
                         embed.set_author(name = author)
                         
-                        embed.add_field(name = "Effect",value = result["text"])
+                        embed.add_field(name = 'Effect',value = result['text'])
 
                         embeds.append(embed)
-                    elif result["category"] == 'planets':
+                    elif result['category'] == 'planets':
                         image = result.get('image')
 
                         embed = discord.Embed(
                             colour=discord.Colour.blue(),
-                            description = "Planet Card",
+                            description = 'Planet Card',
                             title = result['name']
                         )
                         if image:
-                            embed.set_thumbnail(url = result["image"])
+                            embed.set_thumbnail(url = result['image'])
 
                         embed.set_footer(text = footer)
                         embed.set_author(name = author)
                         
-                        embed.add_field(name = "Effect",value = result["text"])
+                        embed.add_field(name = 'Effect',value = result['text'])
 
                         embeds.append(embed)
                     else:
-                        print("Unknown Item")
+                        print('Unknown Item')
                 else:
                     embed = discord.Embed(
                         colour=discord.Colour.darker_grey(),
-                        description = "Item not found in the database",
+                        description = 'Item not found in the database',
                         title = match
                     )
 
@@ -154,6 +154,6 @@ class Client(discord.Client):
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = Client(command_prefix="!", intents=intents)
+client = Client(command_prefix='!', intents=intents)
 client = Client(intents=intents)
 client.run(TOKEN)
